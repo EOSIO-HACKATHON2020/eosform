b6fcba145bef949f614ae25e2ae3359faa458d4b
Olexandr <olex@teeko.io>
2020-05-09 17:45:25 +0300

    added 400, 403, 404, 500 pages handlers and ALLOWED_HOSTS for production

---
+++ base/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class LandingView(TemplateView):
+    template_name = 'landing.html'
+
+
+def page_400(request):
+    return render('400.html', {}, status=400)
+
+
+def page_403(request):
+    return render('403.html', {}, status=403)
+
+
+def page_404(request):
+    return render('404.html', {}, status=404)
+
+
+def page_500(request):
+    return render('500.html', {}, status=500)
--- conf/urls.py
+++ conf/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path
 from django.urls import include
+from base.views import LandingView
 
 admin.site.site_header = settings.ADMIN_SITE_HEADER
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingView.as_view(), name='landing'),
 ]
 
 if settings.DEBUG:
